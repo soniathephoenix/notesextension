a6d1846e9bb1694faa2d68e5c733d7d4bd273b1c
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2024-04-23 20:07:53 +0100

    Add files via upload
    now it saves data on local storage and we can also delete it if no longer want it to render. optimised the render function so that later we will also be able to retrieve the data if we wish to.

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myNotes = [] //array to store the inputed notes
+let oldNotes = [] //array to store the old inputed notes that may have been deleted from the list before
+const inputEl = document.getElementById("input-el") //variable for input elements
+const inputBtn = document.getElementById("input-btn") //variable for save notes button
+const ulEl = document.getElementById("ul-el") //variable for the unordered list that renders the inputed notes
+const deleteBtn = document.getElementById("delete-btn") //variable for delete all button
+const notesFromLocalStorage = JSON.parse( localStorage.getItem("myNotes") ) //variable for the notes stored in the local storage. Parse method used to convert string to object
+console.log(notesFromLocalStorage)
 
-// ["lead1", "lead2"] or null
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
-// 1. Check if leadsFromLocalStorage is truthy
-// 2. If so, set myLeads to its value and call renderLeads()
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads()
+//if statement, stating that if there are notes on the local storage, they will be added to the array which will be rendered to the list
+if (notesFromLocalStorage) {
+    myNotes = notesFromLocalStorage
+    render(myNotes)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-})
-
-function renderLeads() {
+//function to render notes to the unordered list
+function render(notes) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < notes.length; i++) {
         listItems += `
             <li>
                 <p>
-                    ${myLeads[i]}
+                    ${notes[i]}
                 </p>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+//delete button event listener for a double click which will clear data in local storage, in the myNotes array and in the rendered list
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("double clicked!")
+    localStorage.clear()
+    myNotes = []
+    render(myNotes)
+})
+
+//save button event listener for clicks which will push the input value to the local storage
+inputBtn.addEventListener("click", function() {
+    myNotes.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myNotes", JSON.stringify(myNotes) )
+    render(myNotes)
+})
+
+
