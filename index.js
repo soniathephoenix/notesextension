8b3b724348a39718a1c03b4fb734246f99e0f117
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2024-04-26 08:28:04 +0100

    set up copy to clipboard email button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myNotes = [] //array to store the inputed notes
 let oldNotes = [] //array to store the old inputed notes that may have been deleted from the list before
 const inputEl = document.getElementById("input-el") //variable for input elements
 const inputBtn = document.getElementById("input-btn") //variable for save notes button
+const emailBtn = document.getElementById("email-btn") //variable for email notes button *
 const ulEl = document.getElementById("ul-el") //variable for the unordered list that renders the inputed notes
 const deleteBtn = document.getElementById("delete-btn") //variable for delete all button
 const notesFromLocalStorage = JSON.parse( localStorage.getItem("myNotes") ) //variable for the notes stored in the local storage. Parse method used to convert string to object
@@ -46,3 +47,11 @@ inputBtn.addEventListener("click", function() {
 })
 
 
+
+// email button event listener for clicks which will trigger copying the saved content to the clipboard and opening an email so content is ready to be pasted *
+emailBtn.addEventListener("click", function copyText() {
+    /* Copy text from storage into clipboard */
+    navigator.clipboard.writeText
+    (notesFromLocalStorage)
+    
+})
